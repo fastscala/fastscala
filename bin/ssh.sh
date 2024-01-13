9b3e414b4dbdbbef43fe54ccac9b6eaeda573d99
David Antunes <davidbranquinho@gmail.com>
2024-01-13 15:32:24 +0000

    Help files

---
+++ bin/ssh.sh
@@ -0,0 +1,22 @@
+if [[ $1 = *.sh ]];
+then TARGET_SERVER="$1";
+else TARGET_SERVER="default.sh";
+fi
+
+if [[ ! -f "./$TARGET_SERVER" ]];
+then
+echo "ERROR: Please specify target server."
+exit 1
+fi
+
+source $TARGET_SERVER
+source "bin/before.sh"
+
+################################################
+
+DEBUG_RUN=false
+
+RUN "sudo su -"
+
+
+

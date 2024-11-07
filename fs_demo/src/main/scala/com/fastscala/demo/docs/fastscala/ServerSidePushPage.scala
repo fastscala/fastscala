9dc694b6477789a4631250d2404faa41b6f3e8ac
David Miguel Antunes <davidmiguel@antunes.net>
2024-11-07 00:03:24 +0000

    Fix ServerSidePushPage example

--- fs_demo/src/main/scala/com/fastscala/demo/docs/fastscala/ServerSidePushPage.scala
+++ fs_demo/src/main/scala/com/fastscala/demo/docs/fastscala/ServerSidePushPage.scala
@@ -3,6 +3,7 @@ package com.fastscala.demo.docs.fastscala
 import cats.effect.IO
 import com.fastscala.core.FSContext
 import com.fastscala.demo.docs.SingleCodeExamplePage
+import com.fastscala.js.Js
 import com.fastscala.utils.IdGen
 import com.fastscala.xml.scala_xml.JS
 
@@ -19,15 +20,15 @@ class ServerSidePushPage extends SingleCodeExamplePage() {
     val N = 30
 
     def factorial(n: BigInt): IO[BigInt] = for {
-      _ <- IO.sleep(100.millis)
-      _ <- IO(fsc.sendToPage(JS.prepend2(id, div.apply(s"factorial($n)").text_white_50)))
       rslt <- if (n == 0) IO.pure(BigInt(1)) else factorial(n - 1).map(_ * n)
-      _ <- if (n < N) IO(fsc.sendToPage(JS.prepend2(id, div.apply(s"factorial($n) = $rslt").text_white_50))) else IO.unit
+      _ <- if (n < N) IO(fsc.sendToPage(JS.prepend2(id, div.apply(s"factorial($n) = $rslt").text_white_50))).flatMap(_ => IO.sleep(100.millis)) else IO.unit
     } yield rslt
 
     import cats.effect.unsafe.implicits.global
-    val rslt = factorial(N).unsafeRunSync()
-    fsc.sendToPage(JS.prepend2(id, div.apply(s"factorial($N) = $rslt").text_white))
+    factorial(N).unsafeRunAsync({
+      case Right(value) => fsc.sendToPage(JS.prepend2(id, div.apply(s"Finished computation.").text_success_emphasis))
+      case Left(_) => fsc.sendToPage(JS.prepend2(id, div.apply(s"Error!").text_danger))
+    })
 
     h2.apply(s"Calculating the factorial($N)") ++
       <div id={id}></div>.m_2.bg_secondary.p_1
